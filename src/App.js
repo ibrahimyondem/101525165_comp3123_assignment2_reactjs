4de9379d447ab93dd48c60044d4febbf235dea41
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-11-16 23:07:22 -0500

    Configured routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import EmployeeList from './pages/EmployeeList';
+import EmployeeDetails from './pages/EmployeeDetails';
+import EmployeeCreate from './pages/EmployeeCreate';
+import EmployeeEdit from './pages/EmployeeEdit';
+import EmployeeSearch from './pages/EmployeeSearch';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+            {/* Protected Routes */}
+            <Route
+              path="/employees"
+              element={
+                <ProtectedRoute>
+                  <EmployeeList />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/employees/new"
+              element={
+                <ProtectedRoute>
+                  <EmployeeCreate />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/employees/search"
+              element={
+                <ProtectedRoute>
+                  <EmployeeSearch />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/employees/:id"
+              element={
+                <ProtectedRoute>
+                  <EmployeeDetails />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/employees/:id/edit"
+              element={
+                <ProtectedRoute>
+                  <EmployeeEdit />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Default Route */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
